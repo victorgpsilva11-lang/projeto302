542f199c78748bc1793fc1e474eee39dc9d9589c
Victor Gabriel <victorgpsilva11@gmail.com>
2025-10-23 13:58:48 -0300

    Continuação da criação do arquivo da pagina inicial

--- db_connection.php
+++ db_connection.php
@@ -3,8 +3,8 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "todo_list_db_302";
-$conn = new mysqli ($servename, $username, $passaword, $dbname);
-if ($conn => connect_error) {
-    die("Erro na conexão:".$conn => connect_error);
+$conn = new mysqli ($servername, $username, $password, $dbname);
+if ($conn -> connect_error) {
+    die("Erro na conexão:".$conn -> connect_error);
 }
 ?>
--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php include('db_connection.php'); ?>
 <!DOCTYPE html>
 <html lang = "pt-br">
     <head>
@@ -11,5 +12,27 @@
         <input type = "text" name = "title" placeholder = "Nova Tarefa..." required>
         <button lype = "submit">Adicionar</buton>
 </form>
+<hr>
+
+<ul>
+    <?php
+    $sql = "SELECT * FROM tasks ORDER BY id DESC";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+        echo "<li>";
+        echo $row['status'] == 'concluida' ? "<s>{$row['title']}</s>" : $row['title'];
+        echo "
+            <a href= 'update_task.php?id={$row['id']}'>iconv_get_encoding</a>
+            <a href= 'update_task.php?id={$row['id']}'>iconv_strlen</a>";
+        echo "</li>";
+        }
+    } else {
+        echo "<p>Nenhuma tarefa cadastrada.</p>";
+    }
+    ?>
+    </ul>
+ 
 </body>
 </html>
