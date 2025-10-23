0e67744a67310e279d9af1ec22873292e9eb3ea2
Victor Gabriel <victorgpsilva11@gmail.com>
2025-10-23 10:28:47 -0300

    criação da página inicial

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang = "pt-br">
+    <head>
+        <meta charset = "UTF - 8">
+        <title> To_Do_List </title>
+        <link rel = "stylesheet" href = "style.css">
+</head>
+<body>
+    <h1>Minha To Do List</h1>
+    <form action = "add_task.php" method = "POST">
+        <input type = "text" name = "title" placeholder = "Nova Tarefa..." required>
+        <button lype = "submit">Adicionar</buton>
+</form>
+</body>
+</html>
